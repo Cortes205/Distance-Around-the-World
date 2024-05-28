1e2c82eb0ba08e4869985fd40ef60b25529424cf
Alan Cortes <acorte03@uoguelph.ca>
2024-05-27 22:01:06 -0400

    Fixed Search Bar and Enter Button From Being Hidden by Map

--- src/main/App.java
+++ src/main/App.java
@@ -3,17 +3,16 @@ import main.components.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import org.jxmapviewer.*;
-import org.jxmapviewer.viewer.*;
-
 
 @SuppressWarnings("serial")
-public class App extends JFrame implements ComponentListener {
+public class App extends JFrame implements ComponentListener, ActionListener {
 	
 	private SearchBar search;
 	private JButton enter;
 	
-	private JXMapKit map;
+	private Map map;
+	
+	private JLayeredPane screen;
 
 	public App() {
 		
@@ -22,24 +21,23 @@ public class App extends JFrame implements ComponentListener {
 		
 		enter = new JButton();
 		enter.setText("Enter");
-		enter.setBounds(15 + search.getWidth(), 15, 50, search.getHeight());
 		enter.setFocusable(false);
 		enter.setVisible(true);
+		enter.addActionListener(this);
 		
-		map = new JXMapKit();
-		map.setBounds(0, 0, 750, 750);
-		map.setZoomButtonsVisible(true);
-		// City of Toronto as Starting Location
-		map.setAddressLocation(new GeoPosition(43.653225, -79.383186));	
+		map = new Map();
+		
+		screen = new JLayeredPane();
+		screen.add(enter, JLayeredPane.PALETTE_LAYER);
+		screen.add(search, JLayeredPane.PALETTE_LAYER);
+		screen.add(map, JLayeredPane.DEFAULT_LAYER);
 		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("Around the World Calculator");
 		this.setLayout(null);
 		this.setPreferredSize(new Dimension(750, 750));
 		this.setMinimumSize(new Dimension(500, 500));
-		this.add(enter);
-		this.add(search);
-		this.add(map);
+		this.add(screen);
 		this.pack();
 		this.addComponentListener(this);
 		this.setResizable(true);
@@ -51,11 +49,20 @@ public class App extends JFrame implements ComponentListener {
 	public void componentResized(ComponentEvent e) {
 		if (e.getSource() == this) {
 			search.refreshSize();
-			map.setSize(new Dimension(this.getWidth(), this.getHeight()));
 			enter.setBounds(15 + search.getWidth(), 15, 75, search.getHeight());
+			map.setBounds(0, 0, this.getWidth(), this.getHeight());
+			screen.setBounds(0, 0, this.getWidth(), this.getHeight());
 			this.revalidate();
 			this.repaint();
 		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == enter) {
+			search.setInput();
+		}
+		
 	}
 	
 	@Override
