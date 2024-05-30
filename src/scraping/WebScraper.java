fae30216b50457d6123e90622847c2f9a05161d5
Alan Cortes <acorte03@uoguelph.ca>
2024-05-29 20:47:13 -0400

    Brought Function to Searching the Map

--- src/scraping/WebScraper.java
+++ src/scraping/WebScraper.java
@@ -2,6 +2,11 @@ package scraping;
 
 import main.components.Map;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -9,41 +14,73 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class WebScraper {
+public class WebScraper implements ActionListener {
 	
 	private Map map;
 	
+	private Timer waitForSite;
+	
 	private WebDriver page;
+	private WebElement searchField;
+	private WebElement button;
+	private WebElement latitudeField;
+	private WebElement longitudeField;
+	
+	private double latitude;
+	private double longitude;
+	private String title;
 	
 	public WebScraper(Map map, String input) {
 		this.map = map;
 
 		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("--headless=new");
+		options.addArguments("--headless=new");
 		options.addArguments("--deny-permission-prompts");
 		options.addArguments("--disable-popup-blocking");
 		page = new ChromeDriver(options);
 		page.get("https://www.gps-coordinates.net/");
 		
-		WebElement search = page.findElement(By.id("address"));
-		search.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
-		search.clear();
-		search.sendKeys(input);
+		searchField = page.findElement(By.id("address"));
+		searchField.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+		searchField.clear();
+		searchField.sendKeys(input);
 		
-		WebElement button = page.findElement(By.cssSelector(".btn.btn-primary"));
+		button = page.findElement(By.cssSelector(".btn.btn-primary"));
 		button.click();
 		
-		WebElement latitudeField = page.findElement(By.id("latitude"));
-		System.out.println(latitudeField.getAttribute("value"));
-		
-//		double latitude = Double.parseDouble(latitudeField.getText());
-//		
-//		WebElement longitudeField = page.findElement(By.id("latitude"));
-//		double longitude = Double.parseDouble(longitudeField.getText());
-		
-//		System.out.println(latitude + " " + longitude);
-		
-//		map.setWaypoint(latitude, longitude);
+		// Wait for site to update after button click
+		waitForSite = new Timer(0, this);
+		waitForSite.setInitialDelay(1000);
+		waitForSite.setRepeats(false);
+		waitForSite.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == waitForSite) {
+			latitudeField = page.findElement(By.id("latitude"));
+			latitude = Double.parseDouble(latitudeField.getAttribute("value"));
+			
+			longitudeField = page.findElement(By.id("longitude"));
+			longitude = Double.parseDouble(longitudeField.getAttribute("value"));
+						
+			map.setWaypoint(latitude, longitude);
+			title = searchField.getAttribute("value");
+			
+			page.quit();
+		}
+	}
+	
+	public double getLatitude() {
+		return latitude;
+	}
+	
+	public double getLongitude() {
+		return longitude;
+	}
+	
+	public String getTitle() {
+		return title;
 	}
 
 }
